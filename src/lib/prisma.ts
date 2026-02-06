4a3669545230f7f32af32c28918d905e5df57a6c
abir-khan-786 <abirk2day@gmail.com>
2026-02-06 09:14:08 +0600

    server is almost done

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,21 @@
 import "dotenv/config"
-import pg from "pg" // 'pg' লাইব্রেরি ইম্পোর্ট করতে হবে
+import pg from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
-import { prismaAdapter } from "better-auth/adapters/prisma"
 
 const connectionString = `${process.env.DATABASE_URL}`
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-const pool = new pg.Pool({ connectionString })
 
+// ১. পুল এবং অ্যাডাপ্টার তৈরি করুন
+const pool = new pg.Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+// ২. PrismaClient-এর ভেতরে অ্যাডাপ্টারটি পাস করুন
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter: adapter, // এই লাইনটি মিসিং ছিল
     log: ["query"],
   })
 
